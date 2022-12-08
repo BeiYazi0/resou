effc576f799b6f333b5f5a8a02b16cb6a047e206
贝亚子零号 <53272180+BeiYazi0@users.noreply.github.com>
2022-12-08 22:19:31 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+# 热搜+百度百科
+
+一个适用于HoshinoBot的热搜+百度百科插件
+
+### ★ 如果你喜欢的话，请给仓库点一个star支持一下23333 ★
+
+## 本项目地址：
+
+https://github.com/BeiYazi0/resou
+
+## 部署教程：
+
+1.下载或git clone本插件：
+
+在 HoshinoBot\hoshino\modules 目录下使用以下命令拉取本项目
+
+git clone https://github.com/BeiYazi0/resou
+
+2.启用：
+
+在 HoshinoBot\hoshino\config\ **bot**.py 文件的 MODULES_ON 加入 'resou'
+
+然后重启 HoshinoBot
+
+## 指令
+
+【微博/百度/知乎/贴吧热搜】 (+数字）
+
+不加数字默认返回前10个
+
+【百度百科】 +关键词
+
+返回关键词的百度百科词条简介
+
---
+++ resou.py
@@ -0,0 +1,169 @@
+import requests
+import json
+import re
+
+import hoshino
+from hoshino import Service
+from hoshino.typing import CQEvent
+
+sv = Service(
+    name="resou",  # 功能名
+    visible=True,  # 可见性
+    enable_on_default=True,  # 默认启用
+    bundle="工具",  # 分组归类
+    help_='''[微博/百度/知乎/贴吧热搜] (+数字)获取热搜榜(不加数字默认前10位)
+[百度百科] +关键词''',  # 帮助说明
+
+)
+
+
+def render_forward_msg(msg_list: list, uid=2854196306, name='小冰'):
+	forward_msg = []
+	for msg in msg_list:
+		forward_msg.append({
+			"type": "node",
+			"data": {
+				"name": str(name),
+				"uin": str(uid),
+				"content": msg
+			}
+		})
+	return forward_msg
+
+
+@sv.on_prefix('微博热搜')
+async def resou(bot, ev: CQEvent):
+    groupid = ev.group_id
+    content = ev.message.extract_plain_text()
+    if content.isdigit():
+        cnt = min(int(content), 50)
+    else:
+        if content == '':
+            cnt = 10
+        else:
+            return
+    await bot.send(ev,'获取中')
+    url = 'https://weibo.com/ajax/side/hotSearch'
+    srcurl = "https://s.weibo.com/weibo?q="
+    res = requests.get(url)
+    r = res.json()["data"]["realtime"][:cnt]
+    msg_list = ['微博热搜榜']
+    for i,obj in enumerate(r):
+        code = str(obj["word"].encode('utf-8'))[2:-1]
+        code = code.replace('\\x','%')
+        url = srcurl+code
+        result = '%d、%s\nhot:%d\n链接:%s'%(i+1,obj["word"],obj["num"],url)
+        msg_list.append(result)
+    forward_msg = render_forward_msg(msg_list)
+    await bot.send_group_forward_msg(group_id=groupid, messages=forward_msg)
+
+
+@sv.on_prefix('百度热搜')
+async def resou(bot, ev: CQEvent):
+    groupid = ev.group_id
+    content = ev.message.extract_plain_text()
+    if content.isdigit():
+        cnt = min(int(content), 30)
+    else:
+        if content == '':
+            cnt = 10
+        else:
+            return
+    await bot.send(ev,'获取中')
+    url='https://top.baidu.com/board?tab=realtime'
+    res=requests.get(url)
+    r=res.text
+    data = re.search('(<!--s-data:)({.+})(-->)', r)
+    r=json.loads(data.groups()[1])["data"]["cards"][0]["content"][:cnt]
+    msg_list = ['百度热搜榜']
+    for i,obj in enumerate(r):
+        result = '%d、%s\nhot:%s\n链接:%s'%(i+1,obj["desc"],obj["hotScore"],obj["appUrl"])
+        msg_list.append(result)
+    forward_msg = render_forward_msg(msg_list)
+    await bot.send_group_forward_msg(group_id=groupid, messages=forward_msg)
+
+
+@sv.on_prefix('知乎热搜')
+async def resou(bot, ev: CQEvent):
+    groupid = ev.group_id
+    content = ev.message.extract_plain_text()
+    if content.isdigit():
+        cnt = min(int(content), 50)
+    else:
+        if content == '':
+            cnt = 10
+        else:
+            return
+    await bot.send(ev,'获取中')
+    url='https://www.zhihu.com/billboard'
+    res=requests.get(url)
+    r=res.text
+    data = re.search('("hotList":\\[{)(.+?)(}\\])', r)
+    data=data.groups()[1]
+    msg_list=["知乎热搜榜"]
+    for i,obj in enumerate(data.split('},{')[:cnt]):
+        dic=json.loads("{"+obj+"}")["target"]
+        result = '%d、%s\nhot:%s\n链接:%s'%(i+1,dic["titleArea"]["text"],dic["metricsArea"]["text"],dic["link"]["url"])
+        msg_list.append(result)
+    forward_msg = render_forward_msg(msg_list)
+    await bot.send_group_forward_msg(group_id=groupid, messages=forward_msg)
+
+
+@sv.on_prefix('贴吧热搜')
+async def resou(bot, ev: CQEvent):
+    groupid = ev.group_id
+    content = ev.message.extract_plain_text()
+    if content.isdigit():
+        cnt = min(int(content), 30)
+    else:
+        if content == '':
+            cnt = 10
+        else:
+            return
+    await bot.send(ev,'获取中')
+    url='https://tieba.baidu.com/hottopic/browse/topicList'
+    res=requests.get(url)
+    r=res.json()["data"]["bang_topic"]["topic_list"][:cnt]
+    msg_list = ['贴吧热议榜']
+    for i,obj in enumerate(r):
+        result = '%d、%s\nhot:%d\n链接:%s'%(i+1,obj["topic_name"],obj["discuss_num"],obj["topic_url"])
+        msg_list.append(result)
+    forward_msg = render_forward_msg(msg_list)
+    await bot.send_group_forward_msg(group_id=groupid, messages=forward_msg)
+
+
+#下载网页内容
+def download(url):
+    if url is None:
+        return None
+    # 浏览器请求头
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36'
+    headers={'User-Agent':user_agent}
+    r = requests.get(url,headers=headers)
+    if r.status_code == 200:
+        r.encoding = 'utf-8'
+        return r.text
+    return None
+
+
+#提取百科词条简介
+def get_data(html):
+    #regex = re.compile('<meta name="description" content="(.*?)">')
+    regex = re.compile('<div class="lemma-summary" label-module="lemmaSummary">(\s*)<div class="para" label-module="para">([\s\S]*?)</div>(\s*)</div>')
+    data = re.findall(regex, html)[0][1]
+    return data
+
+
+@sv.on_prefix('百度百科')
+async def baikedataget(bot, ev: CQEvent):
+    keyword = ev.message.extract_plain_text()
+    if keyword=='':
+        return
+    url = 'http://baike.baidu.com/item/{}'.format(keyword)
+    html_cont = download(url)
+    try:
+        data = get_data(html_cont)
+        data = re.sub(r'<([\s\S]*?)>|&nbsp;|\n','',data)
+        await bot.send(ev,data,at_sender=True)
+    except Exception as e:
+        await bot.send(ev,f'词条“{keyword}”不存在',at_sender=True) 
